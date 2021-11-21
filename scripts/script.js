6e00afcd552dad9590f0481a70774ec5bf3e1615
Tadesse-Alemayehu <email24t@gmail.com>
2021-11-21 23:59:42 +0300

    a simple event listenet and some style

--- scripts/script.js
+++ scripts/script.js
@@ -1,21 +1,30 @@
-// let dt = document.getElementsByTagName("dt");
+let dt = document.getElementsByTagName("dt");
+let dd = document.getElementsByTagName("dd");
 
-// for (let i = 0; i < dt.length; i++) {
-//     dt[i].addEventListener("click", (x) => {
-//         let curEle = x.target
-//         removeActiveClass(i);
-//         curEle.classList.add("active");
-//         // console.log();
-//     });
-// }
-// function removeActiveClass(key) {
-//     for (let i = 0; i < dt.length; i++) {
-//         if (i == key)
-//             continue;
-//         dt[i].addEventListener("click", (x) => {
-//             let curEle = x.target
-//             curEle.classList.remove("active");
-//             // console.log();
-//         });
-//     }
-// }
+for (let i = 0; i < dt.length; i++) {
+    dt[i].addEventListener("click", (x) => {
+        let curEle = x.target;
+        // removeActiveClass(i);
+        curEle.classList.add("active");
+        let subContent = dd[i];
+        let icon = curEle.getElementsByTagName("span")[0];
+        subContent.style.position = "relative";
+        subContent.style.display = "block";
+        icon.style.transform = "rotate(180deg)";
+        // console.log(subContent);
+    });
+}
+
+for (let i = 0; i < dt.length; i++) {
+    dt[i].addEventListener("mouseout", (x) => {
+        let curEle = x.target;
+        let subContent = dd[i];
+        let icon = dt[i].getElementsByTagName("span")[0];
+        subContent.style.position = "absolute";
+        subContent.style.display = "none";
+        icon.style.transform = "rotate(0deg)";
+        curEle.classList.remove("active");
+        // console.log("mouse is out");
+    })
+
+}
