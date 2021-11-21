412b3dd3754ae14dc6acbc5e235704c0114abae9
Tadesse-Alemayehu <email24t@gmail.com>
2021-11-21 23:35:30 +0300

    before the js

---
+++ scripts/script.js
@@ -0,0 +1,21 @@
+// let dt = document.getElementsByTagName("dt");
+
+// for (let i = 0; i < dt.length; i++) {
+//     dt[i].addEventListener("click", (x) => {
+//         let curEle = x.target
+//         removeActiveClass(i);
+//         curEle.classList.add("active");
+//         // console.log();
+//     });
+// }
+// function removeActiveClass(key) {
+//     for (let i = 0; i < dt.length; i++) {
+//         if (i == key)
+//             continue;
+//         dt[i].addEventListener("click", (x) => {
+//             let curEle = x.target
+//             curEle.classList.remove("active");
+//             // console.log();
+//         });
+//     }
+// }
